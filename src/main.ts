b1d6b56d1fec1ce93758686cb041c9c64ac811c7
Arthur Knoepflin <arthur@knoepflin.eu>
2020-07-09 17:21:30 +0200

    [~] work with pr

--- src/main.ts
+++ src/main.ts
@@ -8,21 +8,27 @@ async function run() {
     try {
         const token = core.getInput('token', { required: false });
         const filtersInput = core.getInput('filters', { required: true });
-        let filters;
-        if (!Array.isArray(filtersInput)) {
-            filters = [filtersInput];
-        } else {
-            filters = filtersInput;
-        }
+        const filters = filtersInput.split('\n').map(s => s.trim()).filter(s => s.length > 0);
         const changes = getFileChanges(token);
-        console.log(changes);
-        console.log(filters);
+        console.log("changes", changes);
+        console.log("filters", filters);
     } catch (err) {
         core.setFailed(err.message);
     }
 }
 
-async function getFileChanges(token: string): Promise<string[]> {
+async function getFileChanges(token: string): Promise<string[] | null> {
+    if (github.context.eventName === 'pull_request') {
+        const pr = github.context.payload.pull_request as Webhooks.WebhookPayloadPullRequestPullRequest;
+        return token ? await getChangedFilesFromApi(token, pr) : await getChangedFilesFromGit(pr.base.sha);
+    } else if (github.context.eventName === 'push') {
+        return getFileChangesFromPush();
+    } else {
+        throw new Error('This action can be triggered only by pull_request or push event');
+    }
+}
+
+async function getFileChangesFromPush(): Promise<string[]> {
     const push = github.context.payload as Webhooks.WebhookPayloadPush;
 
     if (git.isTagRef(push.ref)) {
@@ -41,6 +47,28 @@ async function getFileChanges(token: string): Promise<string[]> {
 async function getChangedFilesFromGit(ref: string): Promise<string[]> {
     await git.fetchCommit(ref);
     return await git.getChangedFiles(git.FETCH_HEAD);
+}
+
+async function getChangedFilesFromApi(
+    token: string,
+    pullRequest: Webhooks.WebhookPayloadPullRequestPullRequest
+): Promise<string[]> {
+    const client = github.getOctokit(token);
+    const pageSize = 100
+    const files: string[] = []
+    for (let page = 0; page * pageSize < pullRequest.changed_files; page++) {
+        const response = await client.pulls.listFiles({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: pullRequest.number,
+            page,
+            per_page: pageSize
+        })
+        for (const row of response.data) {
+            files.push(row.filename)
+        }
+    }
+    return files
 }
 
 run();
