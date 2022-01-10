b56f4aa3f6d4cbecee4c4a29133c27d668a76c15
Jeremy PLANCKEEL <78090694+jplanckeel@users.noreply.github.com>
2022-01-10 17:34:14 +0100

    feat(nodejs): propose working directory
    we want use this action on monorepo with subfolder

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,14 @@ async function run() {
     try {
         const output: string[] = [];
         const token = core.getInput('token', {required: false});
-        const filtersInput = core.getInput('filters', {required: true});
+        const filtersInput = core.getInput('filters', {required: true})
+        const working_directory = core.getInput('working_directory', {required: false})                                      
+        const regex = RegExp(`^${working_directory}\/`);
         const filters = filtersInput.split('\n').map(s => s.trim()).filter(s => s.length > 0);
         const changes = await getFileChanges(token);
         filters.forEach((filter) => {
-            const matchList = match(changes, filter);
+            const changesWorking = changes.map((line) => line.replace(regex, ""));
+            const matchList = match(changesWorking, filter);
             matchList.forEach(potentialMatch => {
                 const baseFolder = potentialMatch.split("/")[0];
                 if (output.indexOf(baseFolder) === -1) {
